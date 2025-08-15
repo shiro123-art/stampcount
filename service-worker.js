1604a4f1b5e12ed4e884e499f7d08f535931bc80
shiro123-art <xiaoshiro97@gmail.com>
2025-08-15 11:58:25 +0900

    Update service-worker.js
    Signed-off-by: shiro123-art <xiaoshiro97@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'stamp-counter-v50';
+const CACHE_NAME = 'stamp-counter-v51';
 const URLS_TO_CACHE = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (e) => {
